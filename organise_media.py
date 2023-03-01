a7149e220ed45916ff9503afddea49edfce0a5a7
TiagoBaiao <baiao.tm@gmail.com>
2023-03-01 00:40:35 +0000

    Add logging to the organise_media script

--- organise_media.py
+++ organise_media.py
@@ -2,16 +2,30 @@ import os
 import sys
 import time
 import shutil
+import logging
+
 
 DIR_TO_ORGANISE = 'C:/test1/test2/test3'
 MEDIA_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.mp4']
+
+# Configure the logger to write to a file and to the console
+def config_logger():
+  logging.basicConfig(
+    level = logging.INFO,
+    format = '%(asctime)s [%(levelname)s] %(message)s',
+    datefmt = '%Y-%m-%d %H:%M:%S',
+    handlers = [
+      logging.FileHandler("organize_media.log"),
+      logging.StreamHandler(sys.stdout)
+    ]
+  )
 
 # Get the list of media files from the input directory path, based on the MEDIA_EXTENSIONS array
 def files(path):
   for file in os.listdir(path):
     if os.path.isfile(os.path.join(path, file)):
       filename, file_extension = os.path.splitext(file)
-      
+
       if file_extension in MEDIA_EXTENSIONS:
         yield file
 
@@ -27,13 +41,14 @@ def safe_move(src_path, dest_path):
     f_name = split_f_name[0] + '_copy.' + split_f_name[1]
 
   if duplicated_f_name:
-    print('Duplicated filename in destination directory: "' + dest_path + '".\n  Renamed a file to: ' + f_name)
+    logging.warning('Duplicated filename in destination directory: "' + dest_path + '".\n  Renamed a file to: ' + f_name)
 
   shutil.move(src_path, os.path.join(dest_path, f_name))
 
 # Iterate over all files in the directory to organise
 def organise_media():
   file_count = 0
+  logging.info('Starting to organise files in "' + DIR_TO_ORGANISE + '"')
 
   for file in files(DIR_TO_ORGANISE):
       creation_epoch = os.stat(os.path.join(DIR_TO_ORGANISE, file)).st_mtime
@@ -50,14 +65,27 @@ def organise_media():
       safe_move(file_path, destination_path)
       file_count += 1
 
-  print('\nFinished moving ' + str(file_count) + ' files!')
+  logging.info('Finished moving ' + str(file_count) + ' files!')
 
-# Confirmation prompt
-answer = input('\nWill organise all files in: "' + DIR_TO_ORGANISE + '" by creation_date in "year/month/" directories.\nType [yes] to continue, or something else to abort\n\n>> ')
-if answer.lower() in ["yes"]:
-  print(' ')
-  organise_media()
-  input('\nPress any key to exit...')
-else:
-  input('\nScript aborted. Press any key to exit...')
-  sys.exit()
+# Main script logic
+def main():
+  # Confirmation prompt
+  answer = input('\nWill organise all files in: "' + DIR_TO_ORGANISE + '" by creation_date in "year/month/" directories.\nType [yes] to continue, or something else to abort\n\n>> ')
+
+  if answer.lower() in ["yes"]:
+    print(' ')
+    config_logger()
+
+    try:
+      organise_media()
+
+    except Exception as e:
+      logging.error(e, exc_info=True)
+
+    input('\nPress any key to exit...')
+
+  else:
+    input('\nScript aborted. Press any key to exit...')
+
+# Run the script
+main()
