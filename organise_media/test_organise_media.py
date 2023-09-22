c7a42cb54f5f9e21a5883249257d8d04042f8d60
Tiago Baião <baiao.tm@gmail.com>
2023-09-22 20:55:46 +0100

    T8: Add tests for is_valid_config and read_config_file in organise_media

--- organise_media/test_organise_media.py
+++ organise_media/test_organise_media.py
@@ -1,7 +1,8 @@
 import os
 import pytest
 import shutil
-from organise_media import get_media_files, safe_move
+import yaml
+from organise_media import is_valid_config, read_config_file, get_media_files, safe_move
 
 @pytest.fixture
 def setup_test_environment():
@@ -10,6 +11,139 @@ def setup_test_environment():
     os.mkdir(test_dir)
     yield test_dir
     shutil.rmtree(test_dir)
+
+def test_is_valid_config_returns_false_for_wrong_input_type():
+    config = ["folders_to_organise", "media_extensions"]
+    valid, error_msg = is_valid_config(config)
+
+    assert not valid
+    assert error_msg == 'The file "config.yaml" must have configuration variables defined. Script aborted.'
+
+def test_is_valid_config_returns_false_for_empty_config():
+    config = {}
+    valid, error_msg = is_valid_config(config)
+
+    assert not valid
+    assert error_msg == 'The configuration file "config.yaml" must have a variable folders_to_organise of type list. Script aborted.'
+
+def test_is_valid_config_returns_false_when_missing_folders_to_organise():
+    config = { 'media_extensions': list() }
+    valid, error_msg = is_valid_config(config)
+
+    assert not valid
+    assert error_msg == 'The configuration file "config.yaml" must have a variable folders_to_organise of type list. Script aborted.'
+
+def test_is_valid_config_returns_false_when_missing_media_extensions():
+    config = { 'folders_to_organise': list() }
+    valid, error_msg = is_valid_config(config)
+
+    assert not valid
+    assert error_msg == 'The configuration file "config.yaml" must have a variable media_extensions of type list. Script aborted.'
+
+def test_is_valid_config_returns_false_when_folders_to_organise_is_of_wrong_type():
+    config = { 'folders_to_organise': {}, 'media_extensions': list() }
+    valid, error_msg = is_valid_config(config)
+
+    assert not valid
+    assert error_msg == 'The configuration file "config.yaml" must have a variable folders_to_organise of type list. Script aborted.'
+
+def test_is_valid_config_returns_false_when_media_extensions_is_of_wrong_type():
+    config = { 'folders_to_organise': list(), 'media_extensions': {} }
+    valid, error_msg = is_valid_config(config)
+
+    assert not valid
+    assert error_msg == 'The configuration file "config.yaml" must have a variable media_extensions of type list. Script aborted.'
+
+def test_is_valid_config_returns_true_when_config_is_complete_with_correct_types():
+    config = { 'folders_to_organise': list(), 'media_extensions': list() }
+    valid, error_msg = is_valid_config(config)
+
+    assert valid
+
+def test_read_config_file_returns_false_for_non_existant_config_file(setup_test_environment):
+    config_file_path = "./test_dir/config.yaml"
+    config, valid, error_msg = read_config_file(config_file_path)
+
+    assert not valid
+    assert error_msg == 'Missing configuration file "config.yaml" in the same directory as "organise_media.py". Script aborted.'
+
+def test_read_config_file_returns_false_for_empty_yaml_file(setup_test_environment):
+    config_file_path = "./test_dir/config.yaml"
+    create_test_folders_and_file_from_path(config_file_path, [])
+
+    config, valid, error_msg = read_config_file(config_file_path)
+
+    assert not valid
+    assert error_msg == 'The file "config.yaml" must have configuration variables defined. Script aborted.'
+
+def test_read_config_file_returns_false_for_invalid_yaml_file(setup_test_environment):
+    config_file_path = "./test_dir/config.yaml"
+    create_test_folders_and_file_from_path(
+        config_file_path,
+        [
+            "media_extensions:  - .jpg  - .png"
+        ]
+    )
+
+    config, valid, error_msg = read_config_file(config_file_path)
+
+    assert not valid
+    assert isinstance(error_msg, yaml.YAMLError)
+
+def test_read_config_file_returns_false_for_yaml_file_misisng_folders_to_organise(setup_test_environment):
+    config_file_path = "./test_dir/config.yaml"
+    create_test_folders_and_file_from_path(
+        config_file_path,
+        [
+            "media_extensions:\n",
+            "    - .jpg\n",
+            "    - .png\n"
+        ]
+    )
+
+    config, valid, error_msg = read_config_file(config_file_path)
+
+    assert not valid
+    assert error_msg == 'The configuration file "config.yaml" must have a variable folders_to_organise of type list. Script aborted.'
+
+def test_read_config_file_returns_false_for_yaml_file_misisng_media_extensions(setup_test_environment):
+    config_file_path = "./test_dir/config.yaml"
+    create_test_folders_and_file_from_path(
+        config_file_path,
+        [
+            "folders_to_organise:\n",
+            "    - D:\\test\\data\n",
+            "    - E:\\some\\folder\n"
+        ]
+    )
+
+    config, valid, error_msg = read_config_file(config_file_path)
+
+    assert not valid
+    assert error_msg == 'The configuration file "config.yaml" must have a variable media_extensions of type list. Script aborted.'
+
+def test_read_config_file_returns_true_and_config_matching_yaml_attributes(setup_test_environment):
+    config_file_path = "./test_dir/config.yaml"
+    create_test_folders_and_file_from_path(
+        config_file_path,
+        [
+            "folders_to_organise:\n",
+            "    - D:\\test\\data\n",
+            "    - E:\\some\\folder\n",
+            "\n",
+            "media_extensions:\n",
+            "    - .jpg\n",
+            "    - .png\n"
+        ]
+    )
+
+    config, valid, error_msg = read_config_file(config_file_path)
+
+    assert valid
+    assert config["folders_to_organise"][0] == "D:\\test\\data"
+    assert config["folders_to_organise"][1] == "E:\\some\\folder"
+    assert config["media_extensions"][0] == ".jpg"
+    assert config["media_extensions"][1] == ".png"
 
 def test_get_media_files_returns_zero_files_for_invalid_path(setup_test_environment):
     test_path = "./non_existant_dir/"
@@ -111,9 +245,13 @@ def test_safe_move_doesnt_overwrite_existing_files(setup_test_environment):
     assert os.path.exists(expected_file_path)
 
 # Helper functions
-def create_test_folders_and_file_from_path(path):
+def create_test_folders_and_file_from_path(path, content = None):
     path_dirs, file_name = os.path.split(path)
     os.makedirs(os.path.dirname(path), exist_ok=True)
 
     if file_name != '':
-        open(path, "x")
+        if content is None:
+            open(path, "x")
+        else:
+            with open(path, "w") as fp:
+                fp.writelines(content)
