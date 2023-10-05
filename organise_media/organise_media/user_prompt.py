a6ae7b9a4940b77ebbeacff9a5c3ebabe1d625f3
Tiago Baião <baiao.tm@gmail.com>
2023-10-05 19:11:14 +0100

    T9: Create a log folder if it doesn't yet exist when creating the organise_media script log file

--- organise_media/organise_media/user_prompt.py
+++ organise_media/organise_media/user_prompt.py
@@ -5,6 +5,8 @@ from .configuration_reader import read_config_file
 from .file_operations import organise_media
 
 RELATIVE_CONFIG_FILE_PATH = "../config.yaml"
+RELATIVE_LOG_DIR_PATH = "../log/"
+LOG_FILE_NAME = "organize_media.log"
 
 # Main script logic
 def run():
@@ -38,12 +40,17 @@ def init():
 
 # Configure the logger to write to a file and to the console
 def config_logger():
+  log_dir_path = os.path.join(os.path.dirname(__file__), RELATIVE_LOG_DIR_PATH)
+
+  if not os.path.exists(log_dir_path):
+    os.makedirs(log_dir_path)
+
   logging.basicConfig(
     level = logging.INFO,
     format = '%(asctime)s [%(levelname)s] %(message)s',
     datefmt = '%Y-%m-%d %H:%M:%S',
     handlers = [
-      logging.FileHandler("organize_media.log"),
+      logging.FileHandler(os.path.join(log_dir_path, LOG_FILE_NAME)),
       logging.StreamHandler(sys.stdout)
     ]
   )
